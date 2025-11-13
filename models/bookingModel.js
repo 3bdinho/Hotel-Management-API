2b028a97e29e59ce2da6982f36ebd0d370413192
abdu-1 <abduulrahmanj@gmail.com>
2025-11-13 13:53:05 +0200

    Booking model + schema

---
+++ models/bookingModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Room" },
+    hotelId: { type: mongoose.Schema.Types.ObjectId, ref: "Hotel" },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    checkIn: { type: Date, required: true },
+    checkOut: { type: Date, required: true },
+    totalPrice: Number,
+    status: {
+      type: String,
+      enum: ["Pending", "Confirmed", "Cancelled"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Book", bookingSchema);
