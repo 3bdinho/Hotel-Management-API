464c3e6b1f2f59b65d4ab1a0b44f0c346df4c78e
abdu-1 <abduulrahmanj@gmail.com>
2025-11-19 11:49:10 +0200

    Refactor sendMail

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,31 +1,62 @@
-const nodeMailer = require("nodemailer");
-const asyncHandler = require("express-async-handler");
+// const nodeMailer = require("nodemailer");
 
-exports.sendMail = async (options) => {
+// exports.sendMail = async (options) => {
+//   try {
+//     //1-Create transporter
+//     const transporter = nodeMailer.createTransport({
+//       host: "sandbox.smtp.mailtrap.io", //process.env.MAIL_HOST,
+//       port: 2525, //process.env.MAIL_PORT,
+//       secure: false, //process.env.MAIL_PORT === 465,
+//       auth: {
+//         user: "0b183ab12d6502", //process.env.USERNAME,
+//         pass: "7042a84ee44002", //process.env.PASSWORD,
+//       },
+//     });
+
+//     //2-Email options
+//     const mailOptions = {
+//       from: "Hotel-system <0b183ab12d6502@inbox.mailtrap.io>",
+//       to: "receiver@mailtrap.io", //options.to,
+//       subject: "Booking Test", //options.subject,
+//       html: "<p>This is a test email</p>", //options.html,
+//     };
+
+//     //3-Send mailOptions
+//     const info = await transporter.sendMail(mailOptions);
+//     console.log("Email sent successfully!", info);
+//   } catch (err) {
+//     console.error("Email sending error:", err.message);
+//   }
+// };
+
+const nodeMailer = require("nodemailer");
+
+exports.sendEmail = async (options) => {
   try {
-    //1-Create transporter
+    //1-Create email transporter
+
     const transporter = nodeMailer.createTransport({
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
+      secure: process.env.MAIL_PORT == 465, // true لو 465
       auth: {
-        user: process.env.USERNAME,
-        pass: process.env.PASSWORD,
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
       },
     });
 
-    //2-Email options
+    //2-Define mail options
     const mailOptions = {
-      from: "Hotel-system <progahmedelsayed@gmail.com>",
-      to: options.email,
+      from: `"Hotel App" <Hotel@business.com>`,
+      to: options.to,
       subject: options.subject,
       html: options.html,
     };
 
-    //3-Send mailOptions
-    await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully!");
+    //3-Email sending
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully!", info.messageId);
   } catch (err) {
-    console.error("Email sending error:", err.message);
-    throw new Error("Failed to send email");
+    console.error("Email sending error:", err);
   }
 };
