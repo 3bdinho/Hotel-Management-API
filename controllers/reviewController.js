95f78c5ab1e5973fe4b43b6d0a20ac0eda138cb9
abdu-1 <abduulrahmanj@gmail.com>
2025-11-28 18:39:42 +0200

    Add find one function for review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -11,3 +11,11 @@ exports.createReview = asyncHandler(async (req, res) => {
   const review = await reviewService.createReviewService(userId, roomId, rate, comment);
   res.status(201).json(review);
 });
+
+// @desc    Get single review
+// @route   GET /api/reviews/:id
+// @access  Public
+exports.getReview = asyncHandler(async (req, res) => {
+  const review = await reviewService.findReview(req.params.id);
+  res.status(200).json(review);
+});
